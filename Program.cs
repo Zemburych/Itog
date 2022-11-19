21a220e58ed79399102ea15681e1282b5fc3b256
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-19 10:34:17 +0300

    решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+string[] array1 = new string[5] { "знакомство", "с", "IT", "прошло", "успешно" }; 
+string[] array2 = new string[array1.Length];
+
+void newmas(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+newmas(array1, array2);
+Console.WriteLine("Исходный массив:");
+PrintArray(array1);
+Console.WriteLine("Новый массив:");
+PrintArray(array2);
